b6689526cd6bc9ce9aeda00de39db78db00d70ca
ali nemati chari <alinemati@live.com>
2017-07-25 16:42:36 +0430

    fix bigs

--- routes/v1/post/index.js
+++ routes/v1/post/index.js
@@ -1,31 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mongoose = require('mongoose');
-var Post = mongoose.model('Post');
-var geo=require('geolib');
-var User = mongoose.model('User');
+var mongoose = require("mongoose");
+var Post = mongoose.model("Post");
+var geo = require("geolib");
+var User = mongoose.model("User");
 var radius = 1000;
 
-router.post('/',function(req,res){
-    if(req.user) new Post({text:req.body.text,location:req.location,user:req.user._id})
-    .save((err,post)=>{return res.jsonp({status: 0, text:post.text,location:post.location});});
-    else return res.sendStatus(401);
+router.post("/", function(req, res) {
+  if (req.user)
+    new Post({
+      text: req.body.text,
+      location: req.location,
+      user: req.user._id
+    }).save((err, post) => {
+      return res.jsonp({ status: 0, text: post.text, location: post.location });
+    });
+  else return res.sendStatus(401);
 });
 
-router.get('/',function(req,res){
-    if(!req.user) new User({unique_id:req.unique_id}).save()
-    var nearbyPosts=[];
-    Post.find({},[],{sort:{date_added: -1}},function(err, posts){
-        posts.forEach(function(post) {
-            if (geo.getDistance(req.location , post.location)<radius){
-                nearbyPosts=nearbyPosts.concat(post);
-            }
-        }, this);
-    return res.jsonp({posts: nearbyPosts,status: 0});
-    })
-return;
-})
-
-
+router.get("/", function(req, res) {
+  if (!req.user) new User({ unique_id: req.unique_id }).save();
+  var nearbyPosts = [];
+  Post.find({}, [], { sort: { date_added: -1 } }, function(err, posts) {
+    posts.forEach(function(post) {
+      if (geo.getDistance(req.location, post.location) < radius) {
+        nearbyPosts = nearbyPosts.concat(post);
+      }
+    }, this);
+    return res.jsonp({ posts: nearbyPosts, status: 0 });
+  });
+  return;
+});
 
 module.exports = router;
