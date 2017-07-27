349e005fd461afc6e6e6524a4e6f6f48c08ca504
sh-soltanpour <shahryar2006@gmail.com>
2017-07-27 11:27:23 +0430

    Like model implementation

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema({
     text: {type: String, required: true},
     location: {type: Object, required: true},
     date: {type: Date, required:true, default: Date.now()},
-    user: {type: String, ref: 'User'}
+    user: {type: String, ref: 'User'},
+    likes: {type: Number, default : 0}
 });
 
 module.exports = Schema;
--- routes/v1/post/like.js
+++ routes/v1/post/like.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
+var Like = mongoose.model("Like");
+var Post = mongoose.model("Post");
 
-router.put("/:id",function(req,res,next){
+router.put("/",function(req,res,next){
     console.log("like id",req.params);
     console.log("post id ",req.postId);
+    new Like({user : req.user.id, post : req.postId}).save();
+    Post.findOne({id : req.post_id}, function(err, post){
+        if (err)
+            console.log(err);
+        if (post){
+            post.likes = post.likes + 1;
+            post.save();
+            return res.jsonp("Liked");
+        }
+    });
+    
 });
 module.exports = router;
