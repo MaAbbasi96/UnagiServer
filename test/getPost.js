61dc7fc9008f84dcb50852dd4f5dfda57d6c0d69
Mahdi <ma.abbasi.9675@gmail.com>
2017-07-30 15:29:42 +0430

    Added Unit Test for getPost sendPost like

---
+++ test/getPost.js
@@ -0,0 +1,19 @@
+var supertest = require("supertest");
+var should = require("should");
+var server = supertest.agent("http://localhost:3000");
+
+var testLocation = { latitude: 35.7293757, longitude: 51.4224622 };
+
+describe("Get Post Test", function(){
+    it("Test1", function(done){
+        server.
+        get("/post").
+        set("unique_id", "11111111111111111111111111111152").
+        set("location", JSON.stringify(testLocation)).
+        expect(200).
+        end(function(err, res){
+            res.status.should.equal(200);
+            done();
+        });
+    });
+});
---
+++ test/like.js
@@ -0,0 +1,35 @@
+var supertest = require("supertest");
+var should = require("should");
+var server = supertest.agent("http://localhost:3000");
+
+var testLocation = { latitude: 35.7293757, longitude: 51.4224622 };
+
+describe("Like Test", function(){
+    it("Test1", function(done){
+        server.
+        get("/post").
+        set("unique_id", "11111111111111111111111111111152").
+        set("location", JSON.stringify(testLocation)).
+        expect(200).
+        end(function(err, res){
+            res.status.should.equal(200);
+            server.
+            put("/post/" + res.body.posts[0]._id + "/like").
+            set("unique_id", "11111111111111111111111111111152").
+            set("location", JSON.stringify(testLocation)).
+            expect(200).
+            end(function(err2, res2){
+                res2.status.should.equal(200);
+                server.
+                delete("/post/" + res.body.posts[0]._id + "/like").
+                set("unique_id", "11111111111111111111111111111152").
+                set("location", JSON.stringify(testLocation)).
+                expect(200).
+                end(function(err3, res3){
+                    res3.status.should.equal(200);
+                    done();
+                });
+            });
+        });
+    });
+});
