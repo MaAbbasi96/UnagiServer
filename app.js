11458b685274a832a2dd029c4916036a257ae63d
Mahdi <ma.abbasi.9675@gmail.com>
2017-07-30 15:55:44 +0430

    Uncomment Mahdiam:D

--- app.js
+++ app.js
@@ -27,8 +27,6 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-//Mahdiam :D
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 var cors = require("cors");
