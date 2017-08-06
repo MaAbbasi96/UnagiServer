f118d67c7f26f801c055d600522c1a46f304fb20
Amirali Esmaeili <esmailiamirali@gmail.com>
2017-08-06 16:14:58 +0430

    Added api-hardstop & updated shell readme

--- shell/README.md
+++ shell/README.md
@@ -1,3 +1,4 @@
 api-prepiare -- starts ngninx and mongodb service
 api-start -- checks for ngninx and mongodb then starts the api from json thorugh pm2 
 api-stop -- stops API, MongoDB and NginX
+api-hardstop -- deletes API from pm2 list , stops ngninx and mongodb
--- shell/api-hardstop.sh
+++ shell/api-hardstop.sh
@@ -2,6 +2,8 @@
 
 #consts
 APP_NAME=UnagiAPI
+MONGODB_SERVICE=mongod.service
+NGINX_SERVICE=nginx.service
 
 #get api server current status
 pm2 describe $APP_NAME &>/dev/null 
@@ -11,7 +13,12 @@ APP_RUNNING=$?
 if [ "${APP_RUNNING}" = "0" ]
 then
     echo "Stopping API..."
-    pm2 stop ${APP_NAME}
+    pm2 delete ${APP_NAME}
 else
     echo "Couldnt find API proccess in pm2 list"
 fi
+
+#stops nginx and mongodb services
+echo "Stoppping MongoDB And NginX..."
+systemctl stop ${MONGODB_SERVICE}
+systemctl stop ${NGINX_SERVICE}
