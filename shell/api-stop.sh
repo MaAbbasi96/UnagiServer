4783a9e90701b2ad1420f1e7ca165fe09a231bd9
Amirali Esmaeili <esmailiamirali@gmail.com>
2017-08-06 15:35:23 +0430

    added api-stop shell

---
+++ shell/api-stop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#consts
+APP_NAME=www
+MONGODB_SERVICE=mongod.service
+NGINX_SERVICE=nginx.service
+
+#get api server current status
+pm2 describe $APP_NAME &>/dev/null 
+APP_RUNNING=$?
+
+#check if api is in list , then stops api
+if [ "${APP_RUNNING}" = "0" ]
+then
+    pm2 stop ${APP_NAME}
+fi
+
+#stops nginx and mongodb services
+systemctl stop ${MONGODB_SERVICE}
+systemctl stop ${NGINX_SERVICE}
