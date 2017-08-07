4c05c4df2a19c1f9551cefad717163fb37f3b6b9
sh-soltanpour <shahryar.soltanpour@gmail.com>
2017-08-07 16:42:09 +0430

    isLiked field bug fix

--- routes/v1/post/like.js
+++ routes/v1/post/like.js
@@ -3,32 +3,32 @@ var router = express.Router();
 var mongoose = require("mongoose");
 var Like = mongoose.model("Like");
 var Post = mongoose.model("Post");
-function updatePostLikes(increment,req){
-    Post.findOne({_id : req.postId}, function(err, post){
-        if (err)
-            console.log(err);
-        if (post){
-            increment ? post.likes = post.likes + 1 : post.likes = post.likes - 1;
-            post.save((err,post)=>{
-                if (err)
-                    console.log(err);
-                return;
-            });
-        }
-    });    
+function updatePostLikes(increment, req) {
+  Post.findOne({ _id: req.postId }, function(err, post) {
+    if (err) console.log(err);
+    if (post) {
+      increment ? (post.likes = post.likes + 1) : (post.likes = post.likes - 1);
+      post.save((err, post) => {
+        if (err) console.log(err);
+        return;
+      });
+    }
+  });
 }
-router.put("/",function(req,res,next){
-    updatePostLikes(true,req);
-    new Like({user : req.user.id, post : req.postId}).save((err,like)=>res.jsonp(like));
-    
+router.put("/", function(req, res, next) {
+  updatePostLikes(true, req);
+  new Like({ user: req.user._id, post: req.postId }).save((err, like) =>
+    res.jsonp(like)
+  );
 });
-router.delete("/",function(req,res,next){
-    Like.findOneAndRemove({user : req.user.id, post: req.postId},(err,response)=>{
-        if (err)
-            console.log(err);  
-        updatePostLikes(false,req);
-        return res.jsonp(response);
-    });
-
+router.delete("/", function(req, res, next) {
+  Like.findOneAndRemove(
+    { user: req.user._id, post: req.postId },
+    (err, response) => {
+      if (err) console.log(err);
+      updatePostLikes(false, req);
+      return res.jsonp(response);
+    }
+  );
 });
 module.exports = router;
