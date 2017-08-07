e395cec74d21da1974107ae407fa1978af664b00
ali nemati <alinemati@live.com>
2017-08-07 17:40:09 +0430

    login with token without username

--- routes/v1/auth/login.js
+++ routes/v1/auth/login.js
@@ -1,28 +1,27 @@
 var express = require("express"),
-    { createAccessToken, createRefreshToken } = require("./tokenGen"),
-    mongoose = require("mongoose"),
-    verifyType = require("./reqVerification");
+  { createAccessToken, createRefreshToken } = require("./tokenGen"),
+  mongoose = require("mongoose"),
+  verifyType = require("./reqVerification");
 var User = mongoose.model("User");
 var router = express.Router();
 
 router.post("/", verifyType, function(req, res) {
-    req.mydata.obj.username = req.body.username;
-    User.findOneAndUpdate(
-        req.mydata.obj,
-        { $set: { refreshtoken: createRefreshToken() } },
-        { new: true },
-        (err, user) => {
-            if (user) {
-                return res.status(201).send({
-                    accesstoken: createAccessToken(user),
-                    refreshtoken: user.refreshtoken,
-                    message: "ok"
-                });
-            } else {
-                return res.status(401).send({ message: "fail" });
-            }
-        }
-    );
+  User.findOneAndUpdate(
+    req.mydata.obj,
+    { $set: { refreshtoken: createRefreshToken() } },
+    { new: true },
+    (err, user) => {
+      if (user) {
+        return res.status(201).send({
+          accesstoken: createAccessToken(user),
+          refreshtoken: user.refreshtoken,
+          message: "ok"
+        });
+      } else {
+        return res.status(401).send({ message: "fail" });
+      }
+    }
+  );
 });
 
 module.exports = router;
