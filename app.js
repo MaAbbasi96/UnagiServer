a775fc685043f35cf4a55090525a3fda8cc3ec41
Amirali Esmaeili <esmailiamirali@gmail.com>
2017-08-07 16:30:40 +0430

    codes formmated using prettier

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require("express"),
-  path = require("path"),
-  favicon = require("serve-favicon"),
-  logger = require("morgan"),
-  cookieParser = require("cookie-parser"),
-  bodyParser = require("body-parser"),
-  mongoose = require("mongoose"),
-  cors = require("cors");
+    path = require("path"),
+    favicon = require("serve-favicon"),
+    logger = require("morgan"),
+    cookieParser = require("cookie-parser"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    cors = require("cors");
 
 mongoose.connect("mongodb://localhost:27017/college", { useMongoClient: true });
 mongoose.Promise = require("bluebird");
@@ -38,9 +38,9 @@ app.use("/", require("./routes/v1"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -48,23 +48,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render("error", {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render("error", {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render("error", {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render("error", {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
--- routes/v1/post/index.js
+++ routes/v1/post/index.js
@@ -10,97 +10,102 @@ var async = require("async");
 var radius = 1000;
 
 router.use("/:id/", function(req, res, next) {
-  req.postId = req.params.id;
-  return next();
+    req.postId = req.params.id;
+    return next();
 });
 router.use("/:id/like", like);
 router.post("/", function(req, res) {
-  if (req.user) {
-    new Post({
-      text: req.body.text,
-      location: req.location,
-      user: req.user._id,
-      date: Date.now()
-    }).save((err, post) => {
-      return res.jsonp({
-        status: 0,
-        text: post.text,
-        location: post.location
-      });
-    });
-  } else return res.sendStatus(401);
+    if (req.user) {
+        new Post({
+            text: req.body.text,
+            location: req.location,
+            user: req.user._id,
+            date: Date.now()
+        }).save((err, post) => {
+            return res.jsonp({
+                status: 0,
+                text: post.text,
+                location: post.location
+            });
+        });
+    } else return res.sendStatus(401);
 });
 router.get("/", function(req, res) {
-  sendPosts(req, res, false);
+    sendPosts(req, res, false);
 });
 router.get("/hot", function(req, res) {
-  sendPosts(req, res, true);
+    sendPosts(req, res, true);
 });
 
 function sendPosts(req, res, hotRequested) {
-  var sortBy;
-  if (hotRequested) sortBy = { hotRate: -1 };
-  else sortBy = { date: -1 };
-  if (!req.user)
-    new User({
-      unique_id: req.unique_id
-    }).save();
-  var nearbyPosts = [];
-  var addPost = false;
-  if (!req.headers.lastpost) {
-    addPost = true;
-  }
-  const cursor = Post.find({}, [], {
-    sort: sortBy
-  }).cursor();
-  cursor
-    .on("data", post => {
-      if (geo.getDistance(req.location, post.location) < radius && addPost) {
-        nearbyPosts = nearbyPosts.concat(post);
-      }
-      if (post._id == req.headers.lastpost) {
+    var sortBy;
+    if (hotRequested) sortBy = { hotRate: -1 };
+    else sortBy = { date: -1 };
+    if (!req.user)
+        new User({
+            unique_id: req.unique_id
+        }).save();
+    var nearbyPosts = [];
+    var addPost = false;
+    if (!req.headers.lastpost) {
         addPost = true;
-      }
-      if (nearbyPosts.length >= 10) addPost = false;
-    })
-    .on("end", () => {
-      if (hotRequested)
-        nearbyPosts.sort((a, b) => {
-          return a.hotRate > b.hotRate ? -1 : b.hotRate > a.hotRate ? 1 : 0;
-        });
-      if (req.user) {
-        //Don't get post likes if user is new
-        async.map(
-          nearbyPosts,
-          (post, cb) => {
-            Like.findOne(
-              {
-                user: req.user._id,
-                post: post.id
-              },
-              (err, like) => {
-                var postObject = post.toObject();
-                like
-                  ? (postObject.isLiked = true)
-                  : (postObject.isLiked = false);
-                cb(null, postObject);
-              }
-            );
-          },
-          (error, response) => {
-            res.jsonp({
-              posts: response,
-              status: 0
-            });
-          }
-        );
-      } else
-        res.jsonp({
-          posts: nearbyPosts,
-          status: 0
-        }); //
-    })
-    .on("error", err => res.jsonp(err));
+    }
+    const cursor = Post.find({}, [], {
+        sort: sortBy
+    }).cursor();
+    cursor
+        .on("data", post => {
+            if (
+                geo.getDistance(req.location, post.location) < radius &&
+                addPost
+            ) {
+                nearbyPosts = nearbyPosts.concat(post);
+            }
+            if (post._id == req.headers.lastpost) {
+                addPost = true;
+            }
+            if (nearbyPosts.length >= 10) addPost = false;
+        })
+        .on("end", () => {
+            if (hotRequested)
+                nearbyPosts.sort((a, b) => {
+                    return a.hotRate > b.hotRate
+                        ? -1
+                        : b.hotRate > a.hotRate ? 1 : 0;
+                });
+            if (req.user) {
+                //Don't get post likes if user is new
+                async.map(
+                    nearbyPosts,
+                    (post, cb) => {
+                        Like.findOne(
+                            {
+                                user: req.user._id,
+                                post: post.id
+                            },
+                            (err, like) => {
+                                var postObject = post.toObject();
+                                like
+                                    ? (postObject.isLiked = true)
+                                    : (postObject.isLiked = false);
+                                cb(null, postObject);
+                            }
+                        );
+                    },
+                    (error, response) => {
+                        res.jsonp({
+                            posts: response,
+                            status: 0
+                        });
+                    }
+                );
+            } else
+                res.jsonp({
+                    posts: nearbyPosts,
+                    status: 0
+                }); //
+        })
+        .on("error", err => res.jsonp(err));
 }
 
 module.exports = router;
