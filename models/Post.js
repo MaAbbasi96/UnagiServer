6115c141df8123ce3c95b1348e7aadb27bec911e
Amirali Esmaeili <esmailiamirali@gmail.com>
2017-08-10 23:09:19 +0430

    Implemented password hashing, register, login

--- models/Post.js
+++ models/Post.js
@@ -1,13 +1,12 @@
-var mongoose = require("mongoose"),
-  mongooseObserver = require("mongoose-observer");
+var mongoose = require("mongoose");
 var User = mongoose.model("User");
 var Schema = mongoose.Schema({
-  text: { type: String, required: true },
-  location: { type: Object, required: true },
-  date: { type: Date, required: true, default: Date.now() },
-  user: { type: String, ref: "User" },
-  likes: { type: Number, default: 0 },
-  hotRate: { type: Number, default: 0 }
+    text: { type: String, required: true },
+    location: { type: Object, required: true },
+    date: { type: Date, required: true, default: Date.now() },
+    user: { type: String, ref: "User" },
+    likes: { type: Number, default: 0 },
+    hotRate: { type: Number, default: 0 }
 });
 
 Schema.index({ date: 1 });
