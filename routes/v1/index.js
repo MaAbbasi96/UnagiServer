c023d63c1c8f2548204e7ebdf137e8e3db2517d1
ali nemati <alinemati@live.com>
2017-08-13 11:26:37 +0430

    verify decoded token

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,31 +1,31 @@
 var express = require("express"),
-  router = express.Router(),
-  post = require("./post"),
-  auth = require("./auth"),
-  jwt = require("express-jwt");
+    router = express.Router(),
+    post = require("./post"),
+    auth = require("./auth"),
+    jwt = require("express-jwt");
 
 router.use("/auth", auth);
 
 router.use(
-  jwt({
-    secret: "oonagi",
-    getToken: function(req) {
-      if (req.headers.accesstoken) return req.headers.accesstoken;
-      return null;
+    jwt({
+        secret: "oonagi",
+        getToken: function(req) {
+            if (req.headers.accesstoken) return req.headers.accesstoken;
+            return null;
+        }
+    }),
+    function(req, res, next) {
+        if (!req.user || !req.user.username) return res.sendStatus(401);
+        if (!req.headers.location) return res.sendStatus(400);
+        try {
+            req.location = JSON.parse(req.headers.location);
+        } catch (err) {
+            return res.sendStatus(400);
+        }
+        if (!req.location.latitude || !req.location.longitude)
+            return res.sendStatus(400);
+        return next();
     }
-  }),
-  function(req, res, next) {
-    if (!req.user) return res.sendStatus(401);
-    if (!req.headers.location) return res.sendStatus(400);
-    try {
-      req.location = JSON.parse(req.headers.location);
-    } catch (err) {
-      return res.sendStatus(400);
-    }
-    if (!req.location.latitude || !req.location.longitude)
-      return res.sendStatus(400);
-    return next();
-  }
 );
 
 router.use("/post", post);
